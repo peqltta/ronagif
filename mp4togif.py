f5dd04de638fe13a138f3e3ca482f3cbaf96ec02
peqltta <aidanpbroadbent@protonmail.com>
2022-04-29 14:03:17 +0100

    fdasfsd

--- mp4togif.py
+++ mp4togif.py
@@ -1,3 +1,4 @@
 import os
 for filename in os.listdir():
-    os.system(f'ffmpeg -i {filename} -vf "fps=24,scale=350:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -r 24 -s 350x350 {filename[:-4]}.gif')
+    if filename[-4:] == ".mp4":
+        os.system(f'ffmpeg -i {filename} -vf "fps=200,scale=1080:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -r 24 -s 1920x1080 {filename[:-4]}.gif')
