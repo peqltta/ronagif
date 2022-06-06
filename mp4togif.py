43c90d9033d9491b2d6b0c541c4f5ba044132461
peqltta <peqlkush@protonmail.com>
2022-06-06 18:52:20 +0100

    unreveal update

--- mp4togif.py
+++ mp4togif.py
@@ -1,4 +1,4 @@
 import os
 for filename in os.listdir():
     if filename[-4:] == ".mp4":
-        os.system(f'ffmpeg -i {filename} -vf "fps=24,scale=1080:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -r 24 -s 350x350 {filename[:-4]}.gif')
+        os.system(f'ffmpeg -i {filename} -vf "fps=24,scale=1400:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -r 24 -s 350x350 {filename[:-4]}.gif')
